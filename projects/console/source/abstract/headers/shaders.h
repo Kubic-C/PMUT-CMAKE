b52077a3c7a064faaa0f85da07479379725e6731
Sawyer <sawyerp06@gmail.com>
2021-01-03 05:22:37 -0600

    Added license to shader.h

--- projects/console/source/abstract/headers/shaders.h
+++ projects/console/source/abstract/headers/shaders.h
@@ -1,3 +1,24 @@
+/*
+    PMUT (Porter Multi Use Terminal) is a multi use command line,
+    that can in the future do networking, file manipulation, and
+    live app manipulation.
+    
+    PMUT  Copyright (C) 2021 Sawyer Porter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+*/
 #ifndef SHADERS_H
 #define SHADERS_H
 
@@ -76,6 +97,8 @@ namespace abstract
     private:
         unsigned int id;
     };
+
+
 }
 
 #endif
